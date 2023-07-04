ee4a24a7254890bcad4ed2cbe766ef85d3bc6cac
RohitTawade <rohittavade54@gmail.com>
2023-07-04 16:07:48 +0530

    connecting to DB

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
-const express = require('express');
-const serverConfig = require('./configs/server.config');
+const express  = require('express');
+const serverConfig =require('./configs/server.config');
+const mongoose = require('mongoose');
+const dbConfig = require('./configs/db.config');
+const userModel = require('./models/user.model');
+const bcrypt = require('bcrypt');
 
 const app = express();
+/**
+ * Logic to connect to MongoDB and create an ADMIN user
+ * Need to have the mongodb up and running in your local machine
+ */
+mongoose.connect(dbConfig.DB_URL);
+const db = mongoose.connection ;
 
-app.listen(serverConfig.PORT, () => {
-    console.log(`server started on the port number ${serverConfig.PORT} `);
+db.on("error", ()=>{
+    console.log("Error while connecting to DB");
+});
+
+db.once("open", ()=>{
+    console.log("DB is connected");
 })
+    
+    /**
+     * Check if the admin user is already present
+     */
+    
 
+app.listen(serverConfig.PORT, ()=>{
+    console.log(`server started on the port number ${serverConfig.PORT}` );
+})
